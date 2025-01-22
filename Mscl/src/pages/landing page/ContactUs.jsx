fe44d43a72f065e4cbea484dcd6b6709701a42b6
kasi-k <kasivishwanath698@gmail.com>
2025-01-22 18:29:13 +0530

    resposiveness

--- Mscl/src/pages/landing page/ContactUs.jsx
+++ Mscl/src/pages/landing page/ContactUs.jsx
@@ -4,6 +4,7 @@ import MsclMain1 from "../../assets/Msclmain1.png";
 const ContactUs = ({ language }) => {
   const translations = {
     en: {
+      title: "Enquiry Form",
       name: "Name",
       phonenumber: "Phone number",
       emailid: "Email Id",
@@ -12,46 +13,54 @@ const ContactUs = ({ language }) => {
       submit: "Submit",
     },
     ta: {
+      title: "வினவல் படிவம்",
       name: "பெயர்",
-      phonenumber: "  தொலைபேசி எண்",
-      emailid: "மின்னஞ்சல் அடைவு ",
+      phonenumber: "தொலைபேசி எண்",
+      emailid: "மின்னஞ்சல் அடைவு",
       subject: "பொருள்",
-      messages: "செய்திகள் ",
-      submit: "அனுப்பவும் ",
+      messages: "செய்திகள்",
+      submit: "அனுப்பவும்",
     },
   };
 
   return (
-    <div className="flex justify-between items-center">
-      <div className="w-2/5 mx-6">
-        <img src={MsclMain1} alt="Madurai Image"/>
+    <div className="flex flex-col md:flex-row justify-between items-center">
+      {/* Image Section */}
+      <div className="w-full  lg:w-2/5 md:w-4/5 mb-6 md:mb-0 lg:mx-6 md:mx-2">
+        <img src={MsclMain1} alt="Madurai Image" />
       </div>
-      <div className="w-1/2 mx-6">
-        <form className="grid gap-5">
+
+      {/* Form Section */}
+      <div className="w-full  lg:w-1/2 md:w-2/4 lg:mx-6 md:mx-2">
+        <div className="my-8 font-roboto">
+          <p className="text-lg font-medium my-2">{translations[language].title}</p>
+          <hr />
+        </div>
+        <form className="grid gap-4">
           <input
             type="text"
             placeholder={translations[language].name}
-            className="border border-[#B5ADAD] px-6 py-3 rounded-lg"
+            className="border border-gray-300 px-4 py-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
           <input
             type="text"
             placeholder={translations[language].phonenumber}
-            className="border border-[#B5ADAD] px-6 py-3 rounded-lg"
+            className="border border-gray-300 px-4 py-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
           <input
             type="email"
             placeholder={translations[language].emailid}
-            className="border border-[#B5ADAD] px-6 py-3 rounded-lg"
+            className="border border-gray-300 px-4 py-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
           <input
             type="text"
             placeholder={translations[language].subject}
-            className="border border-[#B5ADAD] px-6 py-3 rounded-lg"
+            className="border border-gray-300 px-4 py-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
           <textarea
             rows={5}
             placeholder={translations[language].messages}
-            className="border border-[#B5ADAD] px-6 py-3 rounded-lg"
+            className="border border-gray-300 px-4 py-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
           <button className="place-self-end  bg-[#21409A] px-10 py-2 text-white text-sm rounded-md">
             {translations[language].submit}
--- Mscl/src/pages/landing page/LandingPage.jsx
+++ Mscl/src/pages/landing page/LandingPage.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Logo from "../../assets/logo.png";
-import MsclMain from "../../assets/Msclmain.png";
+import MsclMain from "../../assets/Msclmain.jpg";
 import { TbBulb } from "react-icons/tb";
 import { GrLogin } from "react-icons/gr";
 import { FiFile } from "react-icons/fi";
@@ -8,11 +8,24 @@ import { BsGlobe } from "react-icons/bs";
 import ContactUs from "./ContactUs";
 import Terms from "./Terms";
 import PrivacyPolicy from "./PrivacyPolicy";
-import AboutUs from "./AboutUs";
-import { Link } from "react-router-dom";
+
 const LandingPage = () => {
   const [language, setLanguage] = useState("en");
   const [activeLink, setActiveLink] = useState("");
+
+  useEffect(() => {
+    // Check localStorage for language preference and set the state
+    const savedLanguage = localStorage.getItem("language");
+    if (savedLanguage) {
+      setLanguage(savedLanguage);
+    }
+  }, []);
+
+  const handleLanguageChange = (event) => {
+    const selectedLanguage = event.target.value;
+    setLanguage(selectedLanguage);
+    localStorage.setItem("language", selectedLanguage); // Save the language to localStorage
+  };
 
   const translations = {
     en: {
@@ -99,9 +112,6 @@ const LandingPage = () => {
     },
   };
 
-  const handleLanguageChange = (event) => {
-    setLanguage(event.target.value);
-  };
 
   const handleLinkClick = (link) => {
     setActiveLink(link);
@@ -109,36 +119,32 @@ const LandingPage = () => {
 
   return (
     <>
-      <div className="mx-auto  px-6 font-roboto">
+      <div className="px-6 font-roboto">
         <header className="fixed top-0 left-0 z-10 w-full bg-white bg-opacity-80 px-4">
-          <div className="flex justify-between items-center">
+          <div className="lg:flex md:flex flex lg:justify-between md:justify-between justify-center items-center lg:flex-nowrap md:flex-nowrap flex-wrap gap-4">
             <div className="flex items-center gap-2">
-              <img src={Logo} alt="Madurai city" className="size-20" />
-              <h1 className="text-[#21409A] font-bold text-xl my-4">
+              <img
+                src={Logo}
+                alt="Madurai city"
+                className="lg:size-20 md:size-16 size-12 my-3"
+              />
+              <h1 className="text-[#21409A] font-bold text-xl">
                 {translations[language].title}
               </h1>
             </div>
-            <nav className="flex gap-2 font-normal text-lg items-center cursor-pointer">
+            <nav className="flex gap-3 font-normal text-lg items-center cursor-pointer">
               <a
                 href=""
                 onClick={() => handleLinkClick("")}
-                className={`px-6 py-2 rounded-md ${
+                className={`lg:px-6 md:px-6 px-2 py-2 rounded-md ${
                   activeLink === "" ? "text-white bg-[#21409A]" : "text-black"
                 }`}
               >
                 {translations[language].home}
               </a>
               <a
-                onClick={() => handleLinkClick("/")}
-                className={`px-6 py-2 rounded-lg ${
-                  activeLink === "/" ? "text-white bg-[#21409A]" : "text-black"
-                }`}
-              >
-                {translations[language].dashboard}
-              </a>
-              <a
                 onClick={() => handleLinkClick("/contactus")}
-                className={`px-6 py-2 rounded-lg ${
+                className={`md:px-6 px-2 text-wrap py-2 rounded-lg ${
                   activeLink === "/contactus"
                     ? "text-white bg-[#21409A]"
                     : "text-black"
@@ -163,26 +169,26 @@ const LandingPage = () => {
 
         {/* homepage content */}
         {activeLink === "" && (
-          <main className="pt-20 space-y-40 ">
-            <section className="relative   ">
-              <div>
-                <img src={MsclMain} alt="Madurai city" className="" />
-                <div className="absolute -bottom-32 left-1/2 transform -translate-x-1/2 grid grid-cols-3 gap-6 bg-white p-6 w-4/5 rounded-xl shadow-md">
-                  <button className="px-8 py-8 flex flex-col items-center justify-center gap-2 bg-[#E6D590] rounded-lg">
-                    <div className="bg-[#21409A] rounded-full px-4 py-4 ">
-                      <TbBulb className="size-6 text-[#E6D590]" />
+          <main className="lg:pt-28 md:pt-28 pt-40 lg:space-y-40 md:space-y-48 space-y-28 ">
+            <section className="">
+              <div className="relative ">
+                <img src={MsclMain} alt="Madurai city" className="rounded-[40px]" />
+                <div className="absolute -translate-y-1/3 left-1/2 transform -translate-x-1/2 lg:grid md:grid flex lg:grid-cols-3 md:grid-cols-3  lg:gap-6 md:gap-6 gap-3 bg-white lg:p-6 md:p-6 p-3 lg:w-4/5 md:w-4/5 w-6/6 justify-center items-center  rounded-xl shadow-md px-2">
+                  <button className="lg:px-8 md:px-8 px-1.5 lg:py-8 md:py-8 py-1.5 flex flex-col items-center justify-center gap-1 bg-[#E6D590] rounded-lg lg:h-full md:h-full h-28">
+                    <div className="bg-[#21409A] rounded-full lg:px-4 md:px-4 px-2 lg:py-4 md:py-4 py-2 ">
+                      <TbBulb className="lg:size-6 md:size-6 size-4 text-[#E6D590]" />
                     </div>
                     <p>{translations[language].grievance}</p>
                   </button>
-                  <button className="px-8 py-8 flex flex-col items-center justify-center gap-2 bg-[#E6D590] rounded-lg">
-                    <div className="bg-[#21409A] rounded-full px-4 py-4 ">
-                      <GrLogin className="size-6 text-[#E6D590] " />
+                  <button className="lg:px-8 md:px-8 px-1.5 lg:py-8 md:py-8 py-1.5 flex flex-col items-center justify-center gap-1 bg-[#E6D590] rounded-lg lg:h-full md:h-full  h-28">
+                    <div className="bg-[#21409A] rounded-full lg:px-4 md:px-4 px-2 lg:py-4 md:py-4 py-2 ">
+                      <GrLogin className="lg:size-6 md:size-6 size-4 text-[#E6D590] " />
                     </div>
                     <p>{translations[language].fileGrievance}</p>
                   </button>
-                  <button className="px-8 py-8 flex flex-col items-center justify-center gap-2 bg-[#E6D590] rounded-lg">
-                    <div className="bg-[#21409A] rounded-full px-4 py-4 ">
-                      <FiFile className="size-6 text-[#E6D590]" />
+                  <button className="lg:px-8 md:px-8 px-1.5 lg:py-8 md:py-8 py-1.5 flex flex-col items-center justify-center gap-1 bg-[#E6D590] rounded-lg lg:h-full md:h-full  h-28">
+                    <div className="bg-[#21409A] rounded-full lg:px-4 md:px-4 px-2 lg:py-4 md:py-4 py-2 ">
+                      <FiFile className="lg:size-6 md:size-6 size-4 text-[#E6D590]" />
                     </div>
                     <p>{translations[language].officerLogin}</p>
                   </button>
@@ -194,11 +200,11 @@ const LandingPage = () => {
               <h2 className="font-bold text-2xl mb-8">
                 {translations[language].stepsTitle}
               </h2>
-              <div className="grid grid-cols-5 items-center gap-4 w-4/5 mx-auto ">
+              <div className="grid lg:grid-cols-5 md:grid-cols-5 grid-cols-1 items-center gap-4 lg:w-4/5 md:w-4/5 mx-auto ">
                 {/* Step 1 */}
-                <div className="col-span-2 flex justify-center items-center p-6 bg-[#FFF5CE] rounded-lg">
-                  <div className="bg-white shadow-md px-8 py-10 text-center w-full space-y-2">
-                    <h3 className="font-bold text-xl text-[#21409A]">
+                <div className=" col-span-2 flex justify-center items-center p-6 bg-[#FFF5CE] rounded-lg">
+                  <div className="bg-white shadow-md lg:px-8 md:px-2 px-6 lg:py-10 md:py-10 py-10 text-center w-full space-y-2">
+                    <h3 className="font-bold text-xl text-[#21409A] ">
                       {translations[language].step1.title}
                     </h3>
                     <h1 className="font-semibold text-lg">
@@ -210,15 +216,24 @@ const LandingPage = () => {
                   </div>
                 </div>
 
-                {/* Arrow */}
-                <div className="flex items-center ">
-                  <div className="border-t-2 border-dashed border-gray-600 flex-grow ml-6"></div>
+                {/* Vertical or Horizontal Arrow */}
+
+                {/* Horizontal Arrow for large and medium screens */}
+                <div className="hidden lg:flex md:flex items-center">
+                  <div className="border-t-2 border-dashed border-gray-600 flex-grow "></div>
                   <div className="border-t-[8px] border-b-[8px] border-l-[12px] border-transparent border-l-gray-600"></div>
+                </div>
+                {/* Vertical Arrow for small screens */}
+                <div className="lg:hidden md:hidden flex items-center justify-center lg:w-full md:w-full w-full ">
+                  <div className=" flex flex-col items-center">
+                    <div className="h-12 border-l-2 border-dashed border-gray-600"></div>
+                    <div className="w-0 h-0 border-t-[12px] border-l-[6px] border-r-[6px] border-t-gray-600 border-l-transparent border-r-transparent mt-[-1px]"></div>
+                  </div>
                 </div>
 
                 {/* Step 2 */}
                 <div className="col-span-2 flex justify-center items-center p-6 bg-[#FFF5CE] rounded-lg">
-                  <div className="bg-white shadow-md px-8 py-10 text-center w-full space-y-2">
+                  <div className="bg-white shadow-md lg:px-8 md:px-2  px-6 lg:py-10 md:py-10 py-10  text-center w-full space-y-2">
                     <h3 className="font-bold text-xl text-[#21409A]">
                       {translations[language].step2.title}
                     </h3>
@@ -230,33 +245,24 @@ const LandingPage = () => {
                     </p>
                   </div>
                 </div>
-                {/* Step 4 */}
-                <div className="col-span-2 flex justify-center items-center p-6 bg-[#FFF5CE] rounded-lg">
-                  <div className="bg-white shadow-md px-8 py-10 text-center w-full space-y-2">
-                    <h3 className="font-bold text-xl text-[#21409A]">
-                      {translations[language].step4.title}
-                    </h3>
-                    <h1 className="font-semibold text-lg">
-                      {translations[language].step4.heading}
-                    </h1>
-                    <p className="text-[#575757] font-normal text-sm">
-                      {translations[language].step4.description}
-                    </p>
+
+                {/* Dotted Downward Arrow */}
+                {/* <div className="lg:col-span-6 md:col-span-12 hidden lg:flex md:flex lg:flex-col md:flex-col lg:px-52 md:px-52 lg:items-end md:items-end">
+                  <div className="h-20 border-l-2 border-dashed border-gray-600 px-[2.4px]"></div>
+                  <div className="w-0 h-0 border-t-[12px] border-l-[6px] border-r-[6px] border-t-gray-600 border-l-transparent border-r-transparent mt-[-1px]"></div>
+                </div> */}
+
+                {/* Vertical Arrow for small screens */}
+                <div className="lg:hidden md:hidden flex items-center justify-center lg:w-full md:w-full w-full ">
+                  <div className=" flex flex-col items-center">
+                    <div className="h-12 border-l-2 border-dashed border-gray-600"></div>
+                    <div className="w-0 h-0 border-t-[12px] border-l-[6px] border-r-[6px] border-t-gray-600 border-l-transparent border-r-transparent mt-[-1px]"></div>
                   </div>
                 </div>
 
-    
-                
-
                 {/* Step 3 */}
-                <div className="flex items-center rotate-180 ml-6 ">
-                <div className="border-t-2 border-dashed border-gray-600 flex-grow "></div>
-                  <div className="border-t-[8px] border-b-[8px] border-l-[12px] border-transparent border-l-gray-600 "></div>
-               
-                </div>
-
                 <div className="col-span-2 flex justify-center items-center p-6 bg-[#FFF5CE] rounded-lg">
-                  <div className="bg-white shadow-md px-8 py-10 text-center w-full space-y-2">
+                  <div className="bg-white shadow-md lg:px-8 md:px-2 px-6 lg:py-10 md:py-10 py-10  text-center w-full space-y-2">
                     <h3 className="font-bold text-xl text-[#21409A]">
                       {translations[language].step3.title}
                     </h3>
@@ -268,6 +274,36 @@ const LandingPage = () => {
                     </p>
                   </div>
                 </div>
+
+                {/* Vertical or Horizontal Arrow */}
+
+                {/* Horizontal Arrow for large and medium screens */}
+                <div className="hidden lg:flex md:flex items-center">
+                  <div className="border-t-2 border-dashed border-gray-600 flex-grow"></div>
+                  <div className="border-t-[8px] border-b-[8px] border-l-[12px] border-transparent border-l-gray-600"></div>
+                </div>
+                {/* Vertical Arrow for small screens */}
+                <div className=" lg:hidden md:hidden flex items-center justify-center lg:w-full md:w-full w-full">
+                  <div className=" flex flex-col items-center">
+                    <div className="h-12 border-l-2 border-dashed border-gray-600"></div>
+                    <div className="w-0 h-0 border-t-[12px] border-l-[6px] border-r-[6px] border-t-gray-600 border-l-transparent border-r-transparent mt-[-1px]"></div>
+                  </div>
+                </div>
+
+                {/* Step 4 */}
+                <div className="col-span-2 flex justify-center items-center p-6 bg-[#FFF5CE] rounded-lg">
+                  <div className="bg-white shadow-md lg:px-8 md:px-2 px-6 lg:py-10 md:py-10 py-10  text-center w-full space-y-2">
+                    <h3 className="font-bold text-xl text-[#21409A]">
+                      {translations[language].step4.title}
+                    </h3>
+                    <h1 className="font-semibold text-lg">
+                      {translations[language].step4.heading}
+                    </h1>
+                    <p className="text-[#575757] font-normal text-sm">
+                      {translations[language].step4.description}
+                    </p>
+                  </div>
+                </div>
               </div>
             </section>
           </main>
@@ -275,60 +311,62 @@ const LandingPage = () => {
 
         {/* Contact us content */}
         {activeLink === "/contactus" && (
-          <main className="pt-20">
+          <main className="lg:pt-28 md:pt-28 pt-44">
             <ContactUs language={language} />
           </main>
         )}
         {/* down navbar contents */}
         {activeLink === "/terms" && (
-          <main className="pt-20">
+          <main className="lg:pt-28 md:pt-28 pt-44">
             <Terms language={language} />
           </main>
         )}
         {activeLink === "/privacy" && (
-          <main className="pt-20">
+          <main className="lg:pt-28 md:pt-28 pt-44">
             <PrivacyPolicy language={language} />
-          </main>
-        )}
-        {activeLink === "/aboutus" && (
-          <main className="pt-20">
-            <AboutUs language={language} />
           </main>
         )}
       </div>
       <footer className="mt-12   text-white bg-[#21409A] font-roboto ">
-        <div className="mx-8 py-6 space-y-4">
-          <p className="font-medium text-xl">
-            {translations[language].quickLinks}
-          </p>
-          <nav className="flex justify-between font-normal text-sm text-[#D9D9D9] py-4">
-            <button
-              onClick={() => handleLinkClick("/terms")}
-              className="text-left hover:underline"
-            >
-              {translations[language]?.terms || "Terms and Conditions"}
-            </button>
-            <button
-              onClick={() => handleLinkClick("/privacy")}
-              className="text-left hover:underline"
-            >
-              {translations[language].privacy}
-            </button>
-            <button
-              onClick={() => handleLinkClick("/contactus")}
-              className="text-left hover:underline"
-            >
-              {translations[language].contactUs}
-            </button>
-            <button
-              onClick={() => handleLinkClick("/aboutus")}
-              className="text-left hover:underline"
-            >
-              {translations[language].aboutUs}
-            </button>
-          </nav>
-          <hr className="border-[#8C8C8C] border-0.5 mr-20" />
-          <p className="text-[#8B8B8B] py-3 text-sm font-normal">
+        <div className=" py-6 mx-3  space-y-4">
+         
+          <div className=" grid grid-cols-2  mx-8 ">
+            <div className="sm:col-span-1 col-span-2">
+              <p>Phone: 9585912344</p>
+              <p>
+                E-Mail:{" "}
+                <a href="info@maduraismartcity.com">
+                  info@maduraismartcity.com
+                </a>
+              </p>
+              <p className="flex flex-col">
+                <span>Address: 1255, Madurai Corporation Building,</span>
+                <span> Melur Road, Tallakulam, Madurai, Tamil Nadu, 625020.</span>
+              </p>
+            </div>
+            <nav className="sm:items-end items-start sm:col-span-1 col-span-2 flex flex-col  justify-end text-end font-normal text-sm text-[#D9D9D9] py-4 gap-2">
+              <button
+                onClick={() => handleLinkClick("/terms")}
+                className="text-left hover:underline"
+              >
+                {translations[language]?.terms || "Terms and Conditions"}
+              </button>
+              <button
+                onClick={() => handleLinkClick("/privacy")}
+                className="text-left hover:underline"
+              >
+                {translations[language].privacy}
+              </button>
+              <button
+                onClick={() => handleLinkClick("/contactus")}
+                className="text-left hover:underline"
+              >
+                {translations[language].contactUs}
+              </button>
+            </nav>
+          </div>
+          <hr className="border-[#8C8C8C] border-0.5 mr-2" />
+          <p className="text-white py-3 text-center text-sm font-normal">
             {translations[language].rightsReserved}
           </p>
         </div>
